91b3a4291663620cd9f7475e6f35cf9e749f0d4a
Carlos Andres Aguilar Nieto <caguilar@ec.krugercorp.com>
2020-08-16 19:09:38 -0500

    fix test

---
+++ test/index.js
@@ -0,0 +1,12 @@
+const expect = require("chai").expect;
+const request = require("request");
+
+describe("test unit", function () {
+
+    it("test", function (done) {
+        expect(200).to.equal(200);
+        done();
+    });
+
+});
+
